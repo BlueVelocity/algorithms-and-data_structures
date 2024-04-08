945da634162fc0f8e609def70c1d6cc008c3cef2
BlueVelocity <josephrodney@hotmail.com>
2024-04-08 02:18:44 -0400

    Code hash table in hashMap.js with set and get functions

---
+++ javascript/hashMap.js
@@ -0,0 +1,76 @@
+import {Node, LinkedList} from  "./linkedList.js";
+
+class HashNode extends Node {
+  constructor(key, value) {
+    super(value);
+    this.key = key;
+  }
+}
+
+class HashLinkedList extends LinkedList {
+  append(key, value) {
+    const newNode = new HashNode(key, value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.appendNode(this.root, newNode);
+    }
+  }
+
+  prepend(key, value) {
+    const newNode = new HashNode(key, value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      newNode.next = this.root;
+      this.root = newNode;
+    }
+  }
+
+ getValue(key) {
+    if (this.root === null) return new Error('Empty list');
+    let currentNode = this.root;
+    while(currentNode != null) {
+      if (currentNode.key === key) return currentNode.data;
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+}
+
+class HashMap {
+  constructor() {
+    this.buckets = []; 
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber + hashCode + key.charCodeAt(i)) % 16;
+    }
+
+    return hashCode;
+  }
+
+  set(key, value) {
+    const hashVal = this.hash(key);
+
+    if (this.buckets[hashVal] === undefined) {
+      const list = new HashLinkedList();
+      list.append(key, value);
+      this.buckets[hashVal] = list;
+    } else {
+      this.buckets[hashVal].append(value);
+    }
+  }
+
+  get(key) {
+    const hashVal = this.hash(key);
+
+    return this.buckets[hashVal].getValue(key); 
+  }
+}
