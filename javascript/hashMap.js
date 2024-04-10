181a69a80e3f428f280ac70e3954cbbb53d63964
BlueVelocity <josephrodney@hotmail.com>
2024-04-09 20:50:26 -0400

    Automatically test HashMap, produce entries, and expand buckets once capacity is over load factor in hashMap.js

--- javascript/hashMap.js
+++ javascript/hashMap.js
@@ -1,9 +1,10 @@
-import {Node, LinkedList} from  "./linkedList.js";
+import {LinkedList} from  "./linkedList.js";
 
-class HashNode extends Node {
+class HashNode {
   constructor(key, value) {
-    super(value);
     this.key = key;
+    this.value = value;
+    this.next = null;
   }
 }
 
@@ -33,10 +34,20 @@ class HashLinkedList extends LinkedList {
     if (this.root === null) return new Error('Empty list');
     let currentNode = this.root;
     while(currentNode != null) {
-      if (currentNode.key === key) return currentNode.data;
+      if (currentNode.key === key) return currentNode.value;
       currentNode = currentNode.next;
     }
     return null;
+  }
+
+  getKeysAndValues() {
+    let currentNode = this.root;
+    const keyValuePair = [];
+    while(currentNode != null) {
+      keyValuePair.push([currentNode.key, currentNode.value]);
+      currentNode = currentNode.next;
+    }
+    return keyValuePair;
   }
 }
 
@@ -44,6 +55,8 @@ class HashMap {
   constructor() {
     this.buckets = []; 
     this.bucketsLength = 16;
+    this.capacity = 0;
+    this.loadFactor = 0.75;
   }
 
 //simulates fixed bucket length
@@ -64,28 +77,65 @@ class HashMap {
     return hashCode;
   }
 
+  growBuckets() {
+    this.bucketsLength = this.bucketsLength * 2;
+    this.capacity = 0;
+    const currentEntries = this.entries();
+    this.buckets = [];
+    currentEntries.forEach( (entry) => this.set(entry[0], entry[1]));
+  }
+
   set(key, value) {
     const hashVal = this.hash(key);
     this.accessBucket(hashVal);
+
+    if (this.capacity / this.bucketsLength > this.loadFactor) this.growBuckets();
 
     if (this.buckets[hashVal] === undefined) {
       const list = new HashLinkedList();
       list.append(key, value);
       this.buckets[hashVal] = list;
     } else {
-      this.buckets[hashVal].append(value);
+      this.buckets[hashVal].append(key, value);
     }
+
+    this.capacity++;
   }
 
   get(key) {
     const hashVal = this.hash(key);
     this.accessBucket(hashVal);
 
-    return this.buckets[hashVal].getValue(key); 
+    if (this.buckets[hashVal] != undefined) {
+      return this.buckets[hashVal].getValue(key); 
+    } else {
+      console.error(`No Key: '${key}'`);
+    }
+  }
+
+  entries() {
+    const nodes = [];
+    this.buckets.forEach( (bucketContent) => {
+      if (bucketContent != undefined) {
+        bucketContent.getKeysAndValues().forEach( (node) => {
+          nodes.push(node);
+        }) 
+      }
+    })
+    return nodes;
   }
 }
 
-const hashMap = new HashMap();
+function testHashMap(numberOfPairs) {
+  const hashMap = new HashMap();
 
-hashMap.set('customKey', 'customVal');
-console.log(hashMap)
+  for (let i = 0; i < numberOfPairs; i++) {
+    const key = `Key${i}`, value = `Value${i}`;
+    hashMap.set(key, value);
+  }
+
+  console.log(hashMap)
+  console.log(hashMap.get('Key7'))
+}
+
+testHashMap(62);
