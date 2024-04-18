6950b4015a45b15522e90a3c79191e6d8f46d810
BlueVelocity <josephrodney@hotmail.com>
2024-04-18 14:14:10 -0400

    Add mergesort algorithm

---
+++ javascript/mergeSort.js
@@ -0,0 +1,31 @@
+function mergeSort(arr) {
+  if (arr.length == 1) return arr;
+
+  const leftArr = mergeSort(arr.slice(0, Math.round(arr.length / 2)))
+  const rightArr = mergeSort(arr.slice(Math.round(arr.length / 2), arr.length))
+
+  const newArr = []
+
+  while(leftArr.length > 0 || rightArr.length > 0) {
+    if(leftArr[0] >= rightArr[0] || leftArr.length == 0) {
+      newArr.push(rightArr[0])
+      rightArr.shift()
+    } else {
+      newArr.push(leftArr[0])
+      leftArr.shift()
+    }
+  }
+
+  return newArr
+}
+
+
+function test(arr) {
+  if (arr.length == 1) return arr;
+
+  return test(arr.slice(Math.round(arr.length / 2), arr.length))
+}
+
+const val = [3, 7, 2, 8, 1, 9, 4, 6, 5]
+
+console.log(mergeSort(val))
