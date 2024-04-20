90dafc63822c13aa982c0ff1481eaad3e5b7e368
BlueVelocity <josephrodney@hotmail.com>
2024-04-19 21:53:51 -0400

    Add insert, deleteItem, and find functions to balancedBinaryTree.js

--- javascript/balancedBinaryTree.js
+++ javascript/balancedBinaryTree.js
@@ -7,45 +7,140 @@ function Node(data) {
 }
 
 function Tree() {
+
+  function sort(arr) { //recursive merge sort implementation
+    if (arr.length === 1) return arr;
+    let left = sort(arr.slice(0, Math.round(arr.length / 2)));
+    let right = sort(arr.slice(Math.round(arr.length / 2)));
+    let sortedArr = [];
+
+    while (left.length > 0 || right.length > 0) {
+      if (left[0] === right [0]) {
+        right.shift();
+      } else if (left[0] >= right[0]) {
+        sortedArr.push(right.shift());
+      } else if (right[0] >= left[0]) {
+        sortedArr.push(left.shift());
+      } else if (left.length === 0) {
+        sortedArr = sortedArr.concat(right);
+        right = [];
+      } else {
+        sortedArr = sortedArr.concat(left);
+        left = [];
+      }
+    }
+
+    return sortedArr;
+  }
+
+  function constructTreeRecursive(arr) {
+    if (arr.length === 0) return null;
+
+    const mid = Math.floor(arr.length / 2);
+    const node = Node(arr[mid]);
+
+    node.left = constructTreeRecursive(arr.slice(0, mid));
+    node.right = constructTreeRecursive(arr.slice(mid + 1, arr.length));
+
+    return node;
+  }
+
+  function minValueNode(root) {
+    let parentNode = root;
+    let currentNode = root.right;
+
+    while (currentNode.left != null) {
+      parentNode = currentNode;
+      currentNode = currentNode.left;
+    }
+
+    return {parentNode, minNode: currentNode};
+  }
+
   return {
     buildTree(arr) {
-      this.root = this.constructTreeRecursively(this.mergeSort(arr));
+      this.root = constructTreeRecursive(sort(arr));
     },
-    constructTreeRecursively(arr) {
-      if (arr.length === 0) return null;
 
-      const mid = Math.floor(arr.length / 2);
-      const node = Node(arr[mid]);
+    insert(value) { //iterative approach to inserting a value
+      let currentNode = this.root;
+      const newNode = new Node(value);
 
-      node.left = this.constructTreeRecursively(arr.slice(0, mid));
-      node.right = this.constructTreeRecursively(arr.slice(mid + 1, arr.length));
-
-      return node;
-    },
-    mergeSort(arr) {
-      if (arr.length === 1) return arr;
-      let left = this.mergeSort(arr.slice(0, Math.round(arr.length / 2)));
-      let right = this.mergeSort(arr.slice(Math.round(arr.length / 2)));
-      let sortedArr = [];
-
-      while (left.length > 0 || right.length > 0) {
-        if (left[0] === right [0]) {
-          right.shift();
-        } else if (left[0] >= right[0]) {
-          sortedArr.push(right.shift());
-        } else if (right[0] >= left[0]) {
-          sortedArr.push(left.shift());
-        } else if (left.length === 0) {
-          sortedArr = sortedArr.concat(right);
-          right = [];
+      while (currentNode != newNode) {
+        if (currentNode.data === value) {
+          console.log(`Value ${value} exists!`);
+          break
+        } else if (currentNode.data > value) {
+          if (currentNode.left === null) {
+            currentNode.left = newNode;
+            currentNode = currentNode.left;
+          } else {
+            currentNode = currentNode.left;
+          }
         } else {
-          sortedArr = sortedArr.concat(left);
-          left = [];
+          if (currentNode.right === null)  {
+            currentNode.right = newNode;
+            currentNode = currentNode.right;
+          } else {
+            currentNode = currentNode.right
+          }
+        }
+      }
+    },
+
+    deleteItem(value) { //iterative approach to deleting a value
+      let currentNode = this.root;
+      let parentNode = currentNode;
+
+      while (currentNode !== null) {
+        if (currentNode.data === value) {
+          break;
+        } else if (currentNode.data > value) {
+          parentNode = currentNode;
+          currentNode = currentNode.left;
+        } else {
+          parentNode = currentNode;
+          currentNode = currentNode.right;
         }
       }
 
-      return sortedArr;
+      if (currentNode.data === value) {
+        if (currentNode.left === null && currentNode.right === null) {
+          if (parentNode.left === currentNode) {
+            parentNode.left = null;
+          } else {
+            parentNode.right = null;
+          }
+        }
+
+        if (currentNode.left === null) {
+          currentNode === currentNode.right;
+        } else if (currentNode.right === null) {
+          currentNode === currentNode.left;
+        } else {
+          let minNode = minValueNode(currentNode);
+          currentNode.data = minNode.minNode.data;
+          minNode.parentNode.left = null;
+        }
+      }
     },
+
+    find(value) { //iterative approach to finding a value
+      let currentNode = this.root;
+
+      while (currentNode != null) {
+        if (currentNode.data === value) {
+          break;
+        } else if (currentNode.data > value) {
+          currentNode = currentNode.left;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+
+      return currentNode;
+    },
+
     root: null
   }
 }
@@ -68,5 +163,11 @@ const unsortedArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = Tree();
 
 tree.buildTree(unsortedArray);
+
+tree.insert(2);
+
+prettyPrint(tree.root);
+
+tree.deleteItem(8);
 
 prettyPrint(tree.root);
